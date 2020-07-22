c1dd5ea8e663d5779e9e3c2db7f490ec5e9faaec
Ho WY <nkj21509@Hoteki-MacBook-Pro.local>
2020-07-23 07:49:18 +0800

    create vue prj

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,5 @@ module.exports = {
 		proxy: 'https://nkj21509.github.io/'
 	},
 	lintOnSave: false,
-	publicPath: process.env.NODE_ENV === 'production' ? '/glass-vue/' : '/'
+	publicPath: '/glass-vue/'
 }
