d4fa01002631425d5a4cd807ebe49fca3889c643
Ho WY <nkj21509@Hoteki-MacBook-Pro.local>
2020-07-23 08:20:15 +0800

    edit config

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,5 @@ module.exports = {
 		proxy: 'https://localhost:8080/'
 	},
 	lintOnSave: false,
-	// publicPath:  publicPath: process.env.NODE_ENV === 'production' ? '/glass-vue/' : '/'
+	publicPath: process.env.NODE_ENV === 'production' ? '/glass-vue/' : '/'
 }
